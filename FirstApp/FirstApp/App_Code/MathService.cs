8677bb92530d68abe0879b74dc6059fabeca0030
Aljamil1113 <aarazasaljamil@gmail.com>
2021-05-09 13:59:09 +0800

    First WCF Sample

---
+++ FirstApp/FirstApp/App_Code/MathService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class MathService : IMathService
+{
+    public Complex AddComplex(Complex c1, Complex c2)
+    {
+        Complex c = new Complex();
+        c.Real = c1.Real + c2.Real;
+        c.Imag = c1.Imag + c2.Imag;
+
+        return c;
+    }
+
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ FirstApp/FirstAppClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAppClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.MathServiceClient mc = new localhost.MathServiceClient();
+            Console.WriteLine(mc.Add(10, 20));
+            localhost.Complex c1 = new FirstAppClient.localhost.Complex();
+            localhost.Complex c2 = new FirstAppClient.localhost.Complex();
+            c1.Real = 10; c2.Real = 20;
+            c1.Imag = 20; c2.Imag = 30;
+            localhost.Complex c3 = mc.AddComplex(c1, c2);
+            Console.WriteLine(c3.Real + " " + c3.Imag);
+
+            Console.ReadLine();
+        }
+    }
+}
