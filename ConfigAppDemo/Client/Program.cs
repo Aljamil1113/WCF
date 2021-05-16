3227f1e3b9454a9356c43a0a7e9ed237cd5ea9b7
Aljamil1113 <aarazasaljamil@gmail.com>
2021-05-16 18:24:08 +0800

    Endpoints

--- ConfigAppDemo/Client/Program.cs
+++ ConfigAppDemo/Client/Program.cs
@@ -10,6 +10,9 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            localhosttcp1.Service1Client s = new localhosttcp1.Service1Client("NetTcpBinding_IService1");
+            Console.WriteLine(s.GetData(10));
+            Console.ReadKey();
         }
     }
 }
--- ConfigAppDemo/Server/Program.cs
+++ ConfigAppDemo/Server/Program.cs
@@ -1,15 +1,23 @@
-﻿namespace Server
+﻿using System;
+using WcfLibrary;
+using System.ServiceModel;
+
+namespace Server
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //ServiceHost sh = new ServiceHost(typeof(Service1));
-            //sh.Open();
-            //Console.WriteLine("Service has started.");
-            //Console.WriteLine("Press any key to stop the server.");
-            //System.Console.ReadKey();
-            //sh.Close();
+            ServiceHost sh = new ServiceHost(typeof(Service1));
+
+            //System.ServiceModel.Description.ServiceEndpoint sep;
+            //sep = sh.AddServiceEndpoint(typeof(IService1), new WSHttpBinding(), "");
+
+            sh.Open();
+            Console.WriteLine("Service has started.");
+            Console.WriteLine("Press any key to stop the server.");
+            System.Console.ReadKey();
+            sh.Close();
         }
     }
 }
