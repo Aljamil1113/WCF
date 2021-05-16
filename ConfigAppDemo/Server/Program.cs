68bb404e668a2707d91dce8ac2cfb286ed331caf
Aljamil1113 <aarazasaljamil@gmail.com>
2021-05-16 10:12:24 +0800

    Change WCF Libary

--- ConfigAppDemo/Server/Program.cs
+++ ConfigAppDemo/Server/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Server
+﻿namespace Server
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //ServiceHost sh = new ServiceHost(typeof(Service1));
+            //sh.Open();
+            //Console.WriteLine("Service has started.");
+            //Console.WriteLine("Press any key to stop the server.");
+            //System.Console.ReadKey();
+            //sh.Close();
         }
     }
 }
