a5f386041ef30c058c850b0493a1cd8df9f49242
Aljamil1113 <aarazasaljamil@gmail.com>
2021-06-06 21:49:21 +0800

    MSMQ WCF

---
+++ MSMQNonTransactionalQueueDemoApp/Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Messaging;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queue = ".\\private$\\DemoTx";
+            if (!MessageQueue.Exists(queue))
+                MessageQueue.Create(queue,false);
+            ServiceHost sh = new ServiceHost(typeof(DemoLibrary.Service1));
+            sh.Open();
+            Console.WriteLine("Server Started");
+            Console.WriteLine("Press any key to stop the server");
+            Console.ReadKey();
+            Console.WriteLine("Server Stopped");
+        }
+    }
+}
---
+++ MSMQTransactionalQueueDemoApp/Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TransactionScope ts = new TransactionScope(TransactionScopeOption.Required))
+            {
+                localhost.Service1Client proxy = new Client.localhost.Service1Client();
+                int count;
+                count = 1;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                count = 2;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                count = 3;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                count = 4;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                count = 5;
+                proxy.SendMessage(string.Format("This is {0} message from client", count));
+                ts.Complete();
+            }
+        }
+    }
+}
